619ba17833eaa719b41e215ff1cf67d9762df3db
Your Name <your-email@example.com>
2026-06-18 14:49:14 +0530

    ,

--- krushkal.c
+++ krushkal.c
@@ -8,9 +8,10 @@ void kruskal(int n, int cost[20][20])
         parent[i] = -1;
 
     int ne = 1, mincost = 0;
+
     while (ne < n)
     {
-        int a, b, u, v;
+        int a = -1, b = -1, u = -1, v = -1;
         int min = 999;
 
         for (int i = 0; i < n; i++)
@@ -26,14 +27,21 @@ void kruskal(int n, int cost[20][20])
             }
         }
 
+        if (min == 999)
+        {
+            printf("Graph is disconnected\n");
+            return;
+        }
+
         while (parent[u] != -1)
             u = parent[u];
+
         while (parent[v] != -1)
             v = parent[v];
 
         if (u != v)
         {
-            printf("%d edge (%d, %d) = %d\n", ne++, a, b, min);
+            printf("%d edge (%d,%d) = %d\n", ne++, a, b, min);
             mincost += min;
             parent[v] = u;
         }
@@ -41,26 +49,31 @@ void kruskal(int n, int cost[20][20])
         cost[a][b] = cost[b][a] = 999;
     }
 
-    printf("\nmincost: %d", mincost);
+    printf("\nMinimum cost = %d\n", mincost);
 }
 
 int main()
 {
     int n;
+    printf("Enter number of vertices: ");
     scanf("%d", &n);
 
     int cost[20][20];
+
+    printf("Enter cost matrix:\n");
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0)
-            {
+
+            if (cost[i][j] == 0 || i == j)
                 cost[i][j] = 999;
-            }
         }
     }
 
     kruskal(n, cost);
+
+    return 0;
 }
